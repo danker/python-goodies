07d3848a6d3beeacb77c3e89270e66c214dcab04
Eric Danker <eric@breomedia.com>
2020-08-19 21:53:32 -0500

    added more python stuff

--- json_example.py
+++ json_example.py
@@ -14,3 +14,12 @@ with open(json_data_file, "w") as write_file:
     json.dump(data, write_file, indent=4, separators=(',', ': '), sort_keys=True)
     #add trailing newline for POSIX compatibility
     write_file.write('\n')
+
+# write json to a python string
+# if you don't need to pretty-print, you can skip the "indent/separators/sort_keys" bit
+json_string_ex = json.dumps(data, indent=4, separators=(',', ': '), sort_keys=True)
+print(json_string_ex)
+
+# load json string into python objects
+json_python_structure = json.loads(json_string_ex)
+print(type(json_python_structure))
